3652dd1615708a1e6171064a4927edd5a92b9d75
doaamelhem96 <drduaamelhem@gamil.com>
2023-05-08 11:08:50 +0300

    updatings

--- array-insert-shift/.py
+++ array-insert-shift/.py
@@ -1,14 +1,59 @@
 def insertShiftArray(arr, value):
     n = len(arr)
-    mid = n // 2
+    middle = n // 2
+    array = [None] * (n + 1)
     
-    for i in range(n - 1, mid, -1):
-        arr[i] = arr[i - 1]
+    if n % 2 == 0:
+        i = 0
+        while i < middle:
+            array[i] = arr[i]
+            i += 1
+        
+        array[middle] = value
+        
+        while i < n:
+            array[i + 1] = arr[i]
+            i += 1
+    else:
+        i = 0
+        while i <= middle:
+            array[i] = arr[i]
+            i += 1
+        
+        array[middle + 1] = value
+        
+        while i < n:
+            array[i + 1] = arr[i]
+            i += 1
     
-    arr[mid] = value
+    return array
+
+"""*****"""
+def removeShiftArray(z):
+    n = len(z)
+    middle = n // 2
+    nz = [None] * (n - 1)
+    i = 0
+    j = 0
+
+    if n % 2 == 0:
+        while i < n:
+            if i != middle:
+                nz[j] = z[i]
+                j += 1
+            i += 1
+    else:
+        while i < n:
+            if i != middle + 1:
+                nz[j] = z[i]
+                j += 1
+            i += 1
     
-    return arr
-arr = [1, 2, 4, 5]
-value = 3
-new_arr = insertShiftArray(arr, value)
-print(new_arr)
+    return nz
+
+if __name__=="__main__":
+   arr2 = [2, 4, 6, 7, 8]
+   print(f" my origin array is <<<\t{arr2}\t>>>, My_array will become after insert and shiftting is <<<{insertShiftArray(arr2,0)}>>>\n******")
+   print(f"******\n my origin array is <<<\t{arr2}\t>>> , My_array will become after remove middle element and filling the gaps is <<<{removeShiftArray(arr2)}>>>")
+
+ 
