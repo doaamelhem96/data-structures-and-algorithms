b29d82c8da7352e9e082f1f942a35a93ce5bc244
Dua'a Melhem <drduaamelhem@gamil.com>
2023-07-17 21:57:31 +0300

    updating

--- sorting_comparison/sorting.py
+++ sorting_comparison/sorting.py
@@ -1,43 +1,17 @@
-class Movie:
-    def __init__(self, title, year, genres):
-        self.title = title
-        self.year = year
-        self.genres = genres
+def compare_numbers(a, b):
+    if a == b:
+        return 0
+    if a < b:
+        return -1
+    if a > b:
+        return 1
 
-    def __repr__(self):
-        return f"Movie(title='{self.title}', year={self.year}, genres={self.genres})"
+def compare_strings(a, b):
+    a_lower = a.lower()
+    b_lower = b.lower()
+    if a_lower < b_lower:
+        return -1
+    elif a_lower > b_lower:
+        return 1
+    return 0
 
-
-def sort_movies_by_year(movies):
-    movies.sort(key=lambda movie: movie.year, reverse=True)
-    return movies
-
-
-def sort_movies_by_title(movies):
-    def get_title_key(movie):
-        title = movie.title
-        if title.startswith("A "):
-            return title[2:]
-        elif title.startswith("An "):
-            return title[3:]
-        elif title.startswith("The "):
-            return title[4:]
-        return title
-
-    movies.sort(key=get_title_key)
-    return movies
-
-
-movies = [
-    Movie("The Avengers", 2012, ["Action", "Adventure"]),
-    Movie("Anchorman: The Legend of Ron Burgundy", 2004, ["Comedy"]),
-    Movie("A Beautiful Mind", 2001, ["Biography", "Drama"]),
-]
-
-# Sort movies by year
-sorted_by_year = sort_movies_by_year(movies)
-print(sorted_by_year)
-
-# Sort movies by title
-sorted_by_title = sort_movies_by_title(movies)
-print(sorted_by_title)
--- sorting_comparison/test/test_sorting.py
+++ sorting_comparison/test/test_sorting.py
@@ -1,28 +1,14 @@
-from sorting import Movie, sort_movies_by_year, sort_movies_by_title
 import pytest
+from sorting import *
 
-def test_sort_movies_by_year():
-    movies = [
-        Movie("The Avengers", 2012, ["Action", "Adventure"]),
-        Movie("Anchorman: The Legend of Ron Burgundy", 2004, ["Comedy"]),
-        Movie("A Beautiful Mind", 2001, ["Biography", "Drama"]),
-    ]
-    sorted_movies = sort_movies_by_year(movies)
-    assert sorted_movies == [
-        Movie("The Avengers", 2012, ["Action", "Adventure"]),
-        Movie("Anchorman: The Legend of Ron Burgundy", 2004, ["Comedy"]),
-        Movie("A Beautiful Mind", 2001, ["Biography", "Drama"]),
-    ]
+def test_compare_numbers():
+    assert compare_numbers(10, 5) == 1
+    assert compare_numbers(5, 10) == -1
+    assert compare_numbers(5, 5) == 0
 
-def test_sort_movies_by_title():
-    movies = [
-        Movie("The Avengers", 2012, ["Action", "Adventure"]),
-        Movie("Anchorman: The Legend of Ron Burgundy", 2004, ["Comedy"]),
-        Movie("A Beautiful Mind", 2001, ["Biography", "Drama"]),
-    ]
-    sorted_movies = sort_movies_by_title(movies)
-    assert sorted_movies == [
-        Movie("A Beautiful Mind", 2001, ["Biography", "Drama"]),
-        Movie("Anchorman: The Legend of Ron Burgundy", 2004, ["Comedy"]),
-        Movie("The Avengers", 2012, ["Action", "Adventure"]),
-    ]
+def test_compare_strings():
+    assert compare_strings("apple", "banana") == -1
+    assert compare_strings("banana", "apple") == 1
+    assert compare_strings("apple", "apple") == 0
+    assert compare_strings("Apple", "apple") == 0
+    assert compare_strings("apple", "Apple") == 0
